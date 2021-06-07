664f123d45d317dfdeaf56672fcaf1dfc097562b
Павел Никулин <npv90@mail.ru>
2021-06-07 13:43:45 +0400

    Added 23_3_task_Season

---
+++ 23_3_task_Season/23_3_task_Season.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+
+#define SPRING 0
+#define SUMMER 1
+#define AUTUMN 2
+#define WINTER 3
+
+#define CURRENT_SEASON AUTUMN
+
+int main()
+{
+#if CURRENT_SEASON == 0
+	std::cout << "Current season is spring" << std::endl;
+#elif CURRENT_SEASON == 1
+	std::cout << "Current season is summer" << std::endl;
+#elif CURRENT_SEASON == 2
+	std::cout << "Current season is autumn" << std::endl;
+#elif CURRENT_SEASON == 3
+	std::cout << "Current season is winter" << std::endl;
+#else
+	std::cout << "! Incorrect season !" << std::endl;
+#endif
+
+	system("pause");
+}
