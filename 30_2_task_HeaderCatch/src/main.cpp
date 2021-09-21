56261f94d4cd36b6c4907b3c922662b11f6ec936
Павел Никулин <npv90@mail.ru>
2021-09-21 11:47:26 +0400

    Added 30_2_task_HeaderCatch

---
+++ 30_2_task_HeaderCatch/src/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+#include <cpr/cpr.h>
+
+
+
+int main ()
+{
+    cpr::Response r = cpr::Get (cpr::Url ("http://httpbin.org/html"));
+    size_t h1 = r.text.find ("<h1>") + 4;
+    size_t h1end = r.text.find ("</h1>");
+    std::cout << r.text.substr (h1, h1end - h1) << std::endl;
+}
