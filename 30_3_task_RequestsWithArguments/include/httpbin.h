f9c1a889be35dbdef0b3cd388e38bb87816423c4
Павел Никулин <npv90@mail.ru>
2021-09-21 16:02:12 +0400

    Deleted unusable headers

--- 30_3_task_RequestsWithArguments/include/httpbin.h
+++ 30_3_task_RequestsWithArguments/include/httpbin.h
@@ -1,9 +1,5 @@
 #pragma once
 
-#include <map>
-#include <string>
 
 
 void getTotalRequest ();
-void requestGet ();
-void requestPost ();
--- 30_3_task_RequestsWithArguments/src/httpbin.cpp
+++ 30_3_task_RequestsWithArguments/src/httpbin.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <map>
 #include <vector>
 
 #include <cpr/cpr.h>
@@ -12,6 +11,39 @@
 
 static std::vector <std::string> totalReq;
 static std::vector <std::string> totalReqValue;
+
+
+void requestGet ()
+{
+    std::string args = "?";
+    for (int i = 0; i < totalReq.size (); ++i)
+    {
+        args += totalReq[i] + "=" + totalReqValue[i];
+        if (i + 1 < totalReq.size ())
+            args += "&";
+    }
+
+    std::string urlWithArgs = "http://httpbin.org/get" + args;
+    cpr::Response r = cpr::Get (cpr::Url (urlWithArgs));
+    std::cout << r.text << std::endl;
+}
+
+
+
+void requestPost ()
+{
+    std::string args = "";
+    for (int i = 0; i < totalReq.size (); ++i)
+    {
+        args += totalReq[i] + " " + totalReqValue[i];
+        if (i + 1 < totalReq.size ())
+            args += " ";
+    }
+
+    cpr::Response r = cpr::Post (cpr::Url ("http://httpbin.org/post"),
+                                 cpr::Payload { {"args", args.c_str ()} });
+    std::cout << r.text << std::endl;
+}
 
 
 
@@ -46,45 +78,11 @@ void getTotalRequest ()
                     (req == "post") ? requestPost () : requestGet ();
                     exit = true;
                 }
-                    
+
                 else
                     std::cerr << "! Incorrect request !" << std::endl;
         }
         else
             std::cerr << "! Incorrect request !" << std::endl;
     } while (!exit);
-}
-
-
-
-void requestGet ()
-{
-    std::string args = "?";
-    for (int i = 0; i < totalReq.size (); ++i)
-    {
-        args += totalReq[i] + "=" + totalReqValue[i];
-        if (i + 1 < totalReq.size ())
-            args += "&";
-    }
-    
-    std::string urlWithArgs = "http://httpbin.org/get" + args;
-    cpr::Response r = cpr::Get (cpr::Url (urlWithArgs));
-    std::cout << r.text << std::endl;
-}
-
-
-
-void requestPost ()
-{
-    std::string args = "";
-    for (int i = 0; i < totalReq.size (); ++i)
-    {
-        args += totalReq[i] + " " + totalReqValue[i];
-        if (i + 1 < totalReq.size ())
-            args += " ";
-    }
-        
-    cpr::Response r = cpr::Post (cpr::Url ("http://httpbin.org/post"),
-                                 cpr::Payload { {"args", args.c_str ()} });
-    std::cout << r.text << std::endl;
 }
