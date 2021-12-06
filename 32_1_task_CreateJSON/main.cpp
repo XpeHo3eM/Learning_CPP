be4f7de876789db5122a5f5865f7ac7f9c8641b2
XpeHo3eM <npv90@mail.ru>
2021-12-06 20:32:30 +0400

    Added 32_1_task_CreateJSON

---
+++ 32_1_task_CreateJSON/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <fstream>
+#include "nlohmann/json.hpp"
+
+
+int main()
+{
+    std::ofstream file ("film.json");
+    nlohmann::json myJSON = { 
+        {"name",     "21 jump street"},
+
+        {"country",  "USA"},
+
+        {"date",     "12.03.2012"},
+
+        {"studio",   "Sony Pictures"},
+
+        {"author",   "Jonah Hill"},
+
+        {"director", {"Phil Lord", 
+                      "Christopher Miller"}},
+
+        {"producer", {"Neal H. Moritz", 
+                      "Stephen Cannell"}},
+
+        {"starring", {{"Morton Shmidt", "Jonah Hill"},
+                      {"Greg Jengo",    "Channing Tatum"},
+                      {"Molly",         "Brie Larson"},
+                      {"Eric",          "Dave Franco"}}}
+    };
+
+    file << myJSON;
+}
