f25aa34d3cfc87f4b44ba2fa5d6ebdfe14adb630
XpeHo3eM <npv90@mail.ru>
2022-01-22 12:40:00 +0400

    Added 35_3_task_Filesystem

---
+++ 35_3_task_Filesystem/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <filesystem>
+#include <string>
+
+
+
+namespace fs = std::filesystem;
+
+
+
+int main()
+{
+    std::cout << "Enter path: ";
+    fs::path path;
+    std::cin >> path;
+    std::cout << "Enter extension to find: ";
+    std::string extension;
+    std::cin >> extension;
+    if (extension[0] != '.')
+        extension.insert(extension.begin(), '.');
+
+    auto findFileByExtension = [](fs::path file, const std::string &extension)
+    {
+        const int compareEqual = 0;
+        if (fs::is_regular_file(file) && (file.extension().compare(extension) == compareEqual))
+                std::cout << file << std::endl;
+    };
+
+    for (const auto &f : fs::recursive_directory_iterator(path))
+    {
+        findFileByExtension(f.path(), extension);
+    }        
+}
